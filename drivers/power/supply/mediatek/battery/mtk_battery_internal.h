bde9211542f912077b7a85717bb0445a6c6f7ae1
fukiame <fukiame@proton.me>
2025-01-13 09:23:11 +0700

    drivers/power
    ; Conflicts:
    ;	drivers/power/supply/mediatek/battery/mtk_battery.c
    ;	drivers/power/supply/mediatek/battery/mtk_battery_core.c
    ;	drivers/power/supply/mediatek/battery/mtk_battery_internal.h
    ;	drivers/power/supply/mediatek/charger/mtk_charger.c
    ;	drivers/power/supply/mediatek/charger/mtk_charger_init.h
    ;	drivers/power/supply/mediatek/charger/mtk_charger_intf.h
    ;	drivers/power/supply/mediatek/charger/mtk_chg_type_det.c
    ;	drivers/power/supply/mediatek/charger/mtk_switch_charging.c

--- drivers/power/supply/mediatek/battery/mtk_battery_internal.h
+++ drivers/power/supply/mediatek/battery/mtk_battery_internal.h
@@ -52,7 +52,8 @@
 #define SHUTDOWN_TIME 40
 #define AVGVBAT_ARRAY_SIZE 30
 #define INIT_VOLTAGE 3450
-#define BATTERY_SHUTDOWN_TEMPERATURE 60
+
+#define BATTERY_SHUTDOWN_TEMPERATURE 61
 
 /* ============================================================ */
 /* typedef and Struct*/
@@ -106,7 +107,7 @@ do {\
 	}						\
 } while (0)
 
-#define BM_DAEMON_DEFAULT_LOG_LEVEL 3
+#define BM_DAEMON_DEFAULT_LOG_LEVEL 8
 
 enum gauge_hw_version {
 	GAUGE_HW_V1000 = 1000,
@@ -339,7 +340,6 @@ enum daemon_cmd_int_data {
 	FG_SET_OCV_Vtemp = FG_SET_ANCHOR + 13,
 	FG_SET_OCV_SOC = FG_SET_ANCHOR + 14,
 	FG_SET_CON0_SOFF_VALID = FG_SET_ANCHOR + 15,
-	FG_SET_ZCV_INTR_EN = FG_SET_ANCHOR + 16,
 	FG_SET_DATA_MAX,
 };
 
@@ -669,6 +669,12 @@ struct battery_data {
 	int BAT_batt_temp;
 };
 
+struct bms_data {
+	struct power_supply_desc psd;
+	struct power_supply *psy;
+	struct power_supply_config cfg;
+};
+
 struct BAT_EC_Struct {
 	int fixed_temp_en;
 	int fixed_temp_value;
@@ -789,6 +795,9 @@ struct mtk_battery {
 
 	struct zcv_filter zcvf;
 
+/* Huaqin add for HQ-124361 by miaozhichao at 2021/5/14 start */
+	bool shutdown_delay;
+/* Huaqin add for HQ-124361 by miaozhichao at 2021/5/14 end */
 /*simulator log*/
 	struct simulator_log log;
 
@@ -828,10 +837,13 @@ struct mtk_battery {
 	bool cmd_disable_nafg;
 	bool ntc_disable_nafg;
 
+<<<<<<< HEAD
 /*battery full*/
 	bool is_force_full;
 	int charge_power_sel;
 
+=======
+>>>>>>> 6b21e0a9feb1 (drivers/power)
 /*battery plug out*/
 	bool disable_plug_int;
 /* hwocv swocv */
@@ -892,10 +904,13 @@ struct mtk_battery {
 
 	bool is_reset_aging_factor;
 	int aging_factor;
+<<<<<<< HEAD
 
 	int bat_health;
 	int show_ag;
 	int soc_decimal_rate;
+=======
+>>>>>>> 6b21e0a9feb1 (drivers/power)
 
 	struct timespec uisoc_oldtime;
 
