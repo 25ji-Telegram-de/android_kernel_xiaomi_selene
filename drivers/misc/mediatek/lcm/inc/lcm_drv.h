1ea5788ca6c018c4ce2328555f75783e72363dff
fukiame <fukiame@proton.me>
2025-01-17 08:32:02 +0700

    drivers/misc/mediatek/lcm: import minified OEM changes
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/lcm/inc/lcm_drv.h
+++ drivers/misc/mediatek/lcm/inc/lcm_drv.h
@@ -384,8 +384,8 @@ struct LCM_UFOE_CONFIG_PARAMS {
 /* ------------------------------------------------------------------------- */
 
 struct LCM_DSC_CONFIG_PARAMS {
+	unsigned int ver; /* [7:4] major [3:0] minor */
 	unsigned int slice_width;
-	unsigned int slice_hight;
 	unsigned int bit_per_pixel;
 	unsigned int slice_mode;
 	unsigned int rgb_swap;
@@ -395,6 +395,10 @@ struct LCM_DSC_CONFIG_PARAMS {
 	unsigned int rct_on;
 	unsigned int bp_enable;
 
+	unsigned int pic_height; /* need to check */
+	unsigned int pic_width;  /* need to check */
+	unsigned int slice_height;
+	unsigned int chunk_size;
 	unsigned int dec_delay;
 	unsigned int xmit_delay;
 	unsigned int scale_value;
@@ -409,7 +413,12 @@ struct LCM_DSC_CONFIG_PARAMS {
 
 	unsigned int flatness_minqp;
 	unsigned int flatness_maxqp;
-	unsigned int rc_mode1_size;
+	unsigned int rc_model_size;
+	unsigned int rc_edge_factor;
+	unsigned int rc_quant_incr_limit0;
+	unsigned int rc_quant_incr_limit1;
+	unsigned int rc_tgt_offset_hi;
+	unsigned int rc_tgt_offset_lo;
 };
 
 
@@ -1005,6 +1014,7 @@ struct LCM_DRIVER {
 	void (*set_pwm)(unsigned int divider);
 	unsigned int (*get_pwm)(unsigned int divider);
 	void (*set_backlight_mode)(unsigned int mode);
+	void (*set_hw_info)(void);
 	/* ///////////////////////// */
 
 	int (*adjust_fps)(void *cmdq, int fps, struct LCM_PARAMS *params);
