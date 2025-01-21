c21527a1b849527b27d8c437103af1d5f94ea730
fukiame <fukiame@proton.me>
2025-01-21 15:31:52 +0700

    drivers power supply merge derp

--- drivers/power/supply/mediatek/charger/mtk_switch_charging.c
+++ drivers/power/supply/mediatek/charger/mtk_switch_charging.c
@@ -264,11 +264,12 @@ static void swchg_select_charging_current_limit(struct charger_manager *info)
 		}
 	}
 /* Huaqin add/modify/del for WXYFB-996 by miaozhichao at 2021/3/29 start */
-	} else if (info->chr_type == HVDCP_CHARGER) {
+	else if (info->chr_type == HVDCP_CHARGER) {
 		pdata->input_current_limit = 2000000;
 		pdata->charging_current_limit = 3000000;
+	}
 /* Huaqin add/modify/del for WXYFB-996 by miaozhichao at 2021/3/29 end */
-	} else if (info->chr_type == CHARGING_HOST) {
+	else if (info->chr_type == CHARGING_HOST) {
 		pdata->input_current_limit =
 				info->data.charging_host_charger_current;
 		pdata->charging_current_limit =
