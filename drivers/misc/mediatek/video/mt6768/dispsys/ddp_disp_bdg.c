f93a6cd027ca93a858349b680696781b7cf3f167
fukiame <fukiame@proton.me>
2025-01-23 20:39:27 +0700

    drivers: video: mt6768: revert faulty HQ-135591 merge
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/video/mt6768/dispsys/ddp_disp_bdg.c
+++ drivers/misc/mediatek/video/mt6768/dispsys/ddp_disp_bdg.c
@@ -4700,11 +4700,6 @@ void bdg_first_init(void)
 	EFUSE = (struct BDG_EFUSE_REGS *)DISPSYS_BDG_EFUSE_BASE;
 	GPIO = (struct BDG_GPIO_REGS *)DISPSYS_BDG_GPIO_BASE;
 	TX_CMDQ_REG[0] = (struct DSI_TX_CMDQ_REGS *)(DISPSYS_BDG_TX_DSI0_BASE + 0xd00);
-  
-	/* Huaqin modify for HQ-135591 by caogaojie at 2021/05/15 start */
-	clk_buf_disp_ctrl(true);
-	mdelay(3);
-	/* Huaqin modify for HQ-135591 by caogaojie at 2021/05/15 end */
 
 	spislv_init();
 	spislv_switch_speed_hz(SPI_TX_LOW_SPEED_HZ, SPI_RX_LOW_SPEED_HZ);
@@ -4727,10 +4722,9 @@ int bdg_common_init(enum DISP_BDG_ENUM module,
 	struct LCM_DSI_PARAMS *tx_params;
 
 	DISPFUNCSTART();
- 	clk_buf_disp_ctrl(true);
-	mdelay(1);
+	clk_buf_disp_ctrl(true);
 	bdg_tx_pull_6382_reset_pin();
-	mdelay(3);
+	mdelay(1);
 	spislv_init();
 	spislv_switch_speed_hz(SPI_TX_LOW_SPEED_HZ, SPI_RX_LOW_SPEED_HZ);
 
@@ -4885,12 +4879,6 @@ int bdg_common_init_for_rx_pat(enum DISP_BDG_ENUM module,
 	EFUSE = (struct BDG_EFUSE_REGS *)DISPSYS_BDG_EFUSE_BASE;
 	GPIO = (struct BDG_GPIO_REGS *)DISPSYS_BDG_GPIO_BASE;
 	TX_CMDQ_REG[0] = (struct DSI_TX_CMDQ_REGS *)(DISPSYS_BDG_TX_DSI0_BASE + 0xd00);
-  
-	/* Huaqin modify for HQ-135591 by caogaojie at 2021/05/15 start */
-	clk_buf_disp_ctrl(true);
-	mdelay(5);
-	bdg_tx_pull_6382_reset_pin();
-	/* Huaqin modify for HQ-135591 by caogaojie at 2021/05/15 end */
 
 	bdg_tx_pull_6382_reset_pin();
 
